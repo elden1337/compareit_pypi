d76284237abb17d3ba84e91abe1f8980193dc8b8
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-01-23 14:45:25 +0100

    Initial commit

---
+++ Compare_It/CompareIt.py
@@ -0,0 +1,3 @@
+class CompareIt:
+    pass
+
---
+++ setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="Compare It Homeline",
+    version="0.0.1",
+    author="Magnus Eldén",
+    description="",
+    packages=["Compare_It"]
+)   
