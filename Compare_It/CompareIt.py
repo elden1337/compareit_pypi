2baf68568804179cd6c2046ea91b86d5fbac5acc
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-01-23 15:23:18 +0100

    added gitignore

--- Compare_It/CompareIt.py
+++ Compare_It/CompareIt.py
@@ -1,3 +1,26 @@
+import requests
+
 class CompareIt:
-    pass
+    _at = '' #accesstoken
+    _username = ''
+    _password = ''
+
+    def __init__(self, username, password):
+        self._username = username
+        self._password = password
+
+    def Login(self):
+        print(self._username)
+        pass
+
+    def GetAllEntities(self):
+        pass
+
+    def GetEntity(self, uuid):
+        pass
+
+    def SetEntity(self, uuid, value):
+        pass
+
+
 
