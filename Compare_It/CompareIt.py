c2c49e28a001907232e09902558230ed5b7f3387
magnuselden <magnu@DESKTOP-ACV2GPT>
2022-01-23 20:07:13 +0100

    setup test methods

--- Compare_It/CompareIt.py
+++ Compare_It/CompareIt.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 class CompareIt:
     _at = '' #accesstoken
@@ -11,22 +12,42 @@ class CompareIt:
         self._password = password
 
     def Login(self):
-        print(self._username)
+        uri = self._endpoint + 'user/login'
+        postbody = {"username": self._username, "password": self._password}
+        headers =  {"Content-Type":"application/json"}
+        response = requests.post(uri, data=json.dumps(postbody), headers=headers)
+        self._at = Util.setAccessToken(response.json()['at'])
         pass
 
     def GetAllEntities(self):
         uri = self._endpoint + 'view/overview'
 
-        headers =  {"Content-Type":"application/json", "Authorization": self._at}
-        response = requests.get(uri, headers = headers)
+        if len(self._at) > 1:
+            headers =  {"Content-Type":"application/json", "Authorization": self._at}
+            response = requests.get(uri, headers = headers)
+        else:
+            self.Login()
+            return self.GetAllEntities(self)
 
         if response.status_code == 200:
             return response.json()
         else:
-            return 'error'
+            return 'Error!'
 
     def GetEntity(self, uuid):
-        pass
+        uri = self._endpoint + 'object/'
+
+        if len(self._at) > 1:
+            headers =  {"Content-Type":"application/json", "Authorization": self._at}
+            response = requests.get(uri, headers = headers)
+        else:
+            self.Login()
+            return self.GetEntity(self, uuid)
+
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return 'Error!'
 
     def SetEntity(self, uuid, value):
         pass
@@ -38,5 +59,4 @@ class Util:
 
     @staticmethod
     def setAccessToken(response):
-        at = 'hej'
-        return 'Bearer ' + at
+        return 'Bearer ' + response
